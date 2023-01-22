1c6d38831b0eac2ee5088acf69761f905cbcf335
imattf <imattf@pm.me>
2023-01-22 08:51:59 -0800

    Stubbed out the session service

--- models/session.go
+++ models/session.go
@@ -1,7 +1,26 @@
 package models
 
+import "database/sql"
+
 type Session struct {
 	ID        int
 	UserID    int
+	Token     string //Only set on new session
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+// Create new session for the user provided.
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: Create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }
