3179a7154e8b021cea84a78ff502cea44444b2a4
imattf <imattf@pm.me>
2023-01-30 14:33:48 -0800

    Added context values with types

--- cmd/exp/exp1.go
+++ cmd/exp/exp1.go
@@ -5,10 +5,34 @@ import (
 	"fmt"
 )
 
+type ctxKey string
+
+const favoriteColorKey ctxKey = "favorite-color"
+
 func main() {
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, 123, "blue")
-	value := ctx.Value(123)
-	fmt.Println(value)
-	fmt.Println(ctx)
+
+	// our stored value in the context
+	ctx = context.WithValue(ctx, favoriteColorKey, 123)
+
+	// a different stored value in the same context, with same name-ish
+	// ctx = context.WithValue(ctx, "favorite-color", "red")
+
+	// value1 := ctx.Value(favoriteColorKey)
+	// value2 := ctx.Value("favorite-color")
+	anyValue := ctx.Value(favoriteColorKey)
+
+	// fmt.Println(value1)
+	// fmt.Println(value2)
+
+	// This .(string) is type assertion. Use ok to avoid run-time panic
+	stringValue, ok := anyValue.(string)
+	if !ok {
+		fmt.Println(anyValue, "is not a string")
+		return
+	}
+
+	// fmt.Println(ctx)
+
+	fmt.Println(stringValue, "is a string")
 }
