0b2fed872807271b075e0c730fedc5354c853b58
imattf <imattf@pm.me>
2023-01-30 14:49:18 -0800

    Stored users as context values

--- cmd/exp/exp1.go
+++ cmd/exp/exp1.go
@@ -1,38 +1,50 @@
 package main
 
 import (
-	"context"
+	stdctx "context"
 	"fmt"
+
+	"github.com/imattf/galere/context"
+	"github.com/imattf/galere/models"
 )
 
-type ctxKey string
+// type ctxKey string
 
-const favoriteColorKey ctxKey = "favorite-color"
+// const favoriteColorKey ctxKey = "favorite-color"
 
 func main() {
-	ctx := context.Background()
+	// ctx := context.Background()
+	ctx := stdctx.Background()
+
+	user := models.User{
+		Email: "bob@aol.com",
+	}
 
 	// our stored value in the context
-	ctx = context.WithValue(ctx, favoriteColorKey, 123)
+	// ctx = context.WithValue(ctx, favoriteColorKey, 123)
+	ctx = context.WithUser(ctx, &user)
 
 	// a different stored value in the same context, with same name-ish
 	// ctx = context.WithValue(ctx, "favorite-color", "red")
 
 	// value1 := ctx.Value(favoriteColorKey)
 	// value2 := ctx.Value("favorite-color")
-	anyValue := ctx.Value(favoriteColorKey)
+	// anyValue := ctx.Value(favoriteColorKey)
+	retrievedUser := context.User(ctx)
 
 	// fmt.Println(value1)
 	// fmt.Println(value2)
 
 	// This .(string) is type assertion. Use ok to avoid run-time panic
-	stringValue, ok := anyValue.(string)
-	if !ok {
-		fmt.Println(anyValue, "is not a string")
-		return
-	}
+	// stringValue, ok := anyValue.(string)
+	// if !ok {
+	// 	fmt.Println(anyValue, "is not a string")
+	// 	return
+	// }
 
-	// fmt.Println(ctx)
+	// // fmt.Println(ctx)
 
-	fmt.Println(stringValue, "is a string")
+	// fmt.Println(stringValue, "is a string")
+	fmt.Println(retrievedUser.Email)
+
 }
---
+++ context/users.go
@@ -0,0 +1,27 @@
+package context
+
+import (
+	"context"
+
+	"github.com/imattf/galere/models"
+)
+
+type key string
+
+const (
+	userKey key = "user"
+)
+
+func WithUser(ctx context.Context, user *models.User) context.Context {
+	return context.WithValue(ctx, userKey, user)
+}
+
+func User(ctx context.Context) *models.User {
+	val := ctx.Value(userKey)
+	user, ok := val.(*models.User)
+	if !ok {
+		// guard against nothing being stored in context
+		return nil
+	}
+	return user
+}
