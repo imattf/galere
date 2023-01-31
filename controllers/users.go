02b584f5bab22a9dcbda9d1fe6d8d5149baa1166
imattf <imattf@pm.me>
2023-01-30 18:38:50 -0800

    Added read request context value

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/imattf/galere/context"
 	"github.com/imattf/galere/models"
 )
 
@@ -93,20 +94,29 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
-	// tokenCookie, err := r.Cookie("session")
-	token, err := readCookie(r, CookieSession)
-	if err != nil {
-		fmt.Println(err)
+	ctx := r.Context()
+	user := context.User(ctx)
+	if user == nil {
 		http.Redirect(w, r, "/signin", http.StatusFound)
 		return
 	}
-	// user, err := u.SessionService.User(tokenCookie.Value)
-	user, err := u.SessionService.User(token)
-	if err != nil {
-		fmt.Println(err)
-		http.Redirect(w, r, "/signin", http.StatusFound)
-		return
-	}
+	fmt.Fprintf(w, "Current user: %s\n", user.Email)
+
+	// // tokenCookie, err := r.Cookie("session")
+	// token, err := readCookie(r, CookieSession)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	http.Redirect(w, r, "/signin", http.StatusFound)
+	// 	return
+	// }
+	// // user, err := u.SessionService.User(tokenCookie.Value)
+	// user, err := u.SessionService.User(token)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	http.Redirect(w, r, "/signin", http.StatusFound)
+	// 	return
+	// }
+
 	fmt.Fprintf(w, "Current user: %s\n", user.Email)
 }
 
