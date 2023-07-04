a8335eeeaf626beb7b6db3de111fc34658f4afba
imattf <imattf@pm.me>
2023-07-04 09:53:20 -0700

    Added Update Gallery handler

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -13,8 +13,9 @@ import (
 
 type Galleries struct {
 	Templates struct {
-		New  Template
-		Edit Template
+		New   Template
+		Edit  Template
+		Index Template
 	}
 	GalleryService *models.GalleryService
 }
@@ -85,4 +86,42 @@ func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 	data.ID = gallery.ID
 	data.Title = gallery.Title
 	g.Templates.Edit.Execute(w, r, data)
+}
+
+func (g Galleries) Update(w http.ResponseWriter, r *http.Request) {
+	//Get the ID of the gallery we are editing
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		// 404 error - page isn't found
+		http.Error(w, "Invalid ID", http.StatusNotFound)
+		return
+	}
+	//Query for the gallery
+	gallery, err := g.GalleryService.ByID(id)
+	if err != nil {
+		// if err == models.ErrNotFound {
+		if errors.Is(err, models.ErrNotFound) {
+			http.Error(w, "Gallery not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	//Verify user owns the gallery
+	user := context.User((r.Context()))
+	if gallery.UserID != user.ID {
+		http.Error(w, "You are not authorized to edit this gallery", http.StatusForbidden)
+		return
+	}
+
+	//Parse the title from the form and update the gallery
+	title := r.FormValue("title")
+	gallery.Title = title
+	err = g.GalleryService.Update(gallery)
+	if err != nil {
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
 }
