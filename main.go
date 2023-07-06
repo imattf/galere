49e64587fed32cad2fd158a5cf499d355168b017
imattf <imattf@pm.me>
2023-07-06 10:08:01 -0700

    Added show image handler

--- main.go
+++ main.go
@@ -196,6 +196,7 @@ func main() {
 	// r.Get("/user/{userID}", userHandler)
 	r.Route("/galleries", func(r chi.Router) {
 		r.Get("/{id}", galleriesC.Show)
+		r.Get("/{id}/images/{filename}", galleriesC.Image)
 		r.Group(func(r chi.Router) {
 			r.Use(umw.RequireUser)
 			r.Get("/", galleriesC.Index)
